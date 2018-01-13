b85ca54e8134fd48b2dc32f1cf9782ca42eaec71
Dan <danvossmusic@gmail.com>
2018-01-13 15:02:36 -0500

    Transpose up an octave

--- src/gs-pattern-gen/gs_pattern_gen.cljs
+++ src/gs-pattern-gen/gs_pattern_gen.cljs
@@ -4,45 +4,45 @@
 
 
 ;;define chords
-;;using :C0 for rests 
-(def b-maj [:B3 :C#4 :D#4 :F#4])
-(def g-maj [:G4 :A4 :B4 :D4])
-(def eb-maj [:Eb4 :F4 :G4 :Bb3])
-(def bb-maj [:Bb3 :C4 :D4 :F4])
-(def ab-maj [:Ab4 :Bb4 :C4 :Eb4])
-(def fis-maj [:F#4 :G#4 :A#4 :C#4])
-(def e-maj [:E4 :F#4 :G#4 :B3])
-(def d-maj [:D4 :E4 :F#4 :A4])
-(def c-maj [:C4 :D4 :E4 :G4])
+;;using :C0 for rests
+(def b-maj [:B4 :C#5 :D#5 :F#5])
+(def g-maj [:G5 :A5 :B5 :D5])
+(def eb-maj [:Eb5 :F5 :G5 :Bb4])
+(def bb-maj [:Bb4 :C5 :D5 :F5])
+(def ab-maj [:Ab5 :Bb5 :C5 :Eb5])
+(def fis-maj [:F#5 :G#5 :A#5 :C#5])
+(def e-maj [:E5 :F#5 :G#5 :B4])
+(def d-maj [:D5 :E5 :F#5 :A5])
+(def c-maj [:C5 :D5 :E5 :G5])
 
-(def d-dom [:D4 :E4 :F#4 :A4 :C4 :C0])
-(def bb-dom [:Bb3 :C4 :D4 :F4 :Ab4 :C0])
-(def fis-dom [:F#4 :G#4 :A#3 :C#4 :E4 :C0])
-(def f-dom [:F4 :G4 :A4 :C4 :Eb4 :C0])
-(def eb-dom [:Eb4 :F4 :G4 :Bb4 :Db4 :C0])
-(def db-dom [:Db4 :Eb4 :F4 :Ab4 :B3 :C0])
-(def b-dom [:B3 :C#4 :D#4 :F#4 :A4 :C0])
-(def a-dom [:A4 :B3 :C#4 :E4 :G4 :C0])
-(def g-dom [:G4 :A4 :B3 :D4 :F4 :C0])
+(def d-dom [:D5 :E5 :F#5 :A5 :C5 :C0])
+(def bb-dom [:Bb4 :C5 :D5 :F5 :Ab5 :C0])
+(def fis-dom [:F#5 :G#5 :A#4 :C#5 :E5 :C0])
+(def f-dom [:F5 :G5 :A5 :C5 :Eb5 :C0])
+(def eb-dom [:Eb5 :F5 :G5 :Bb5 :Db5 :C0])
+(def db-dom [:Db5 :Eb5 :F5 :Ab5 :B5 :C0])
+(def b-dom [:B4 :C#5 :D#5 :F#5 :A5 :C0])
+(def a-dom [:A5 :B4 :C#5 :E5 :G5 :C0])
+(def g-dom [:G5 :A5 :B4 :D5 :F5 :C0])
 
-(def a-min7 [:A3 :C4 :E4 :G4])
-(def f-min7 [:F4 :Ab4 :C4 :Eb4])
-(def cis-min7 [:C#4 :E4 :G#4 :B3])
+(def a-min7 [:A4 :C5 :E5 :G5])
+(def f-min7 [:F5 :Ab5 :C5 :Eb5])
+(def cis-min7 [:C#5 :E5 :G#5 :B4])
 
-(def eb-maj7 [:Eb4 :F4 :G4 :Bb3 :C4 :D4 :C0])
-(def b-maj7 [:B3 :C#4 :D#4 :F#4 :G#4 :A#4 :C0])
-(def g-maj7 [:G4 :A4 :B4 :D4 :E4 :F#4 :C0])
-(def d-maj7 [:D4 :E4 :F#4 :A4 :B4 :C#4 :C0])
-(def c-maj7 [:C4 :D4 :E4 :G4 :A4 :B4 :C0])
-(def bb-maj7 [:Bb3 :C4 :D4 :F4 :G4 :A4 :C0])
+(def eb-maj7 [:Eb5 :F5 :G5 :Bb4 :C5 :D5 :C0])
+(def b-maj7 [:B4 :C#5 :D#5 :F#5 :G#5 :A#5 :C0])
+(def g-maj7 [:G5 :A5 :B5 :D5 :E5 :F#5 :C0])
+(def d-maj7 [:D5 :E5 :F#5 :A5 :B5 :C#5 :C0])
+(def c-maj7 [:C5 :D5 :E5 :G5 :A5 :B5 :C0])
+(def bb-maj7 [:Bb4 :C5 :D5 :F5 :G5 :A5 :C0])
 
-(def e-min [:E4 :F#4 :G4 :A4])
-(def d-min [:D4 :E4 :F4 :G4])
-(def c-min [:C4 :D4 :Eb4 :F4])
+(def e-min [:E5 :F#5 :G5 :A5])
+(def d-min [:D5 :E5 :F5 :G5])
+(def c-min [:C5 :D5 :Eb5 :F5])
 
-(def e-dyad [:A4 :C5 :C0])
-(def f-dom7 [:F4 :A4 :C4 :Eb4])
-(def eb-wt [:Eb4 :F4 :G4 :A4 :Db4 :C0])
+(def e-dyad [:A5 :C6 :C0])
+(def f-dom7 [:F5 :A5 :C5 :Eb5])
+(def eb-wt [:Eb5 :F5 :G5 :A5 :Db5 :C0])
 
 
 ;;create individual chord patterns
@@ -98,7 +98,7 @@
     gs-group))
 
 
-;;create countdown pattern 
+;;create countdown pattern
 (defn make-c-group []
   (let [c-group
         (clj->js (flatten (repeatedly 9 #(vec (concat
@@ -137,18 +137,18 @@
 
 
 ;;create bass progressions
-(def gs-bass-group (array 
-                     "B1" "D2" "G2" "Bb2" "Eb2" "Eb3" "A2" "D2"
-                     "G2" "Bb2" "Eb2" "F#2" "B1" "B2" "F2" "Bb2"
-                     "Eb2" "Eb3" "A2" "D2" "G2" "G3" "C#2" "F#2"
-                     "B1" "B2" "F2" "Bb2" "Eb2" "Eb3" "C#3" "F#2"))
-                     
+(def gs-bass-group (array
+                     "B2" "D3" "G3" "Bb3" "Eb3" "Eb4" "A3" "D3"
+                     "G3" "Bb3" "Eb3" "F#3" "B2" "B3" "F3" "Bb3"
+                     "Eb3" "Eb4" "A3" "D3" "G3" "G4" "C#3" "F#3"
+                     "B2" "B3" "F3" "Bb3" "Eb3" "Eb4" "C#4" "F#3"))
 
-(def c-bass-group (array 
-                    "E2" "F2" "Bb1" "Db2" "F#2" "A2" "D2" "D3"
-                    "D2" "Eb2" "Ab2" "B2" "E2" "G2" "C2" "C3"
-                    "C2" "Db2" "F#2" "A2" "D2" "F2" "Bb1" "Bb2"
-                    "E2" "E2" "F2" "F3" "Bb2" "Bb2" "Eb2" "Eb2"))
+
+(def c-bass-group (array
+                    "E3" "F3" "Bb2" "Db3" "F#3" "A3" "D3" "D4"
+                    "D3" "Eb3" "Ab3" "B3" "E3" "G3" "C3" "C4"
+                    "C3" "Db3" "F#3" "A3" "D3" "F3" "Bb2" "Bb3"
+                    "E3" "E3" "F3" "F4" "Bb3" "Bb3" "Eb3" "Eb3"))
 
 
 ;;make the synths
@@ -157,7 +157,7 @@
                       (.toMaster)))
 
 (def bass-synth (-> (new js/Tone.Synth
-                      {:oscillator {:type "square"}}) 
+                      {:oscillator {:type "square"}})
                     (.toMaster)))
 
 
@@ -199,7 +199,7 @@
                           (fn [time note]
                             (.triggerAttackRelease bass-synth note "4n" time))
                           gs-bass-group "2n")
-                        (.start 0))] 
+                        (.start 0))]
     gs-bass-seq
     (set! (.-loop gs-bass-seq) 9))
   (set! (.-value js/Tone.Transport.bpm) 200)
@@ -210,7 +210,7 @@
                          (fn [time note]
                            (.triggerAttackRelease bass-synth note "4n" time))
                          c-bass-group "2n")
-                       (.start 0))] 
+                       (.start 0))]
     c-bass-seq
     (set! (.-loop c-bass-seq) 9))
   (set! (.-value js/Tone.Transport.bpm) 240)
@@ -223,19 +223,19 @@
 
 (defn gs-toggle [a t1 t2]
   (if (= @a t1)
-    (do 
+    (do
       (reset! a t2)
       (gs-play))
-    (do 
+    (do
       (reset! a t1)
       (stop-func))))
 
 (defn c-toggle [a t1 t2]
   (if (= @a t1)
-    (do 
+    (do
       (reset! a t2)
       (c-play))
-    (do 
+    (do
       (reset! a t1)
       (stop-func))))
 
@@ -244,7 +244,7 @@
   (let [button-text (r/atom "Play")]
     (fn []
       [:div
-       [:button 
+       [:button
         {:class "btn btn-default"
          :on-click #(gs-toggle button-text "Play" "Stop")}
         (deref button-text)]])))
@@ -253,7 +253,7 @@
   (let [button-text (r/atom "Play")]
     (fn []
       [:div
-       [:button 
+       [:button
         {:class "btn btn-default"
          :on-click #(c-toggle button-text "Play" "Stop")}
         (deref button-text)]])))
@@ -263,25 +263,23 @@
   [:div {:style {:text-align "center"}}
    [:h3 "Chromatic mediant pattern generator"]
    [:br]
-   [:p "Giant Steps (J. Coltrane)"]
+   [:p "Giant Steps (John Coltrane)"]
    [:b {:class "btn btn-default"
         :on-click gs-generate-pattern}
     "Generate"]
    [gs-button]
    [:br]
    [:br]
-   [:p "Countdown (J. Coltrane)"]
+   [:p "Countdown (John Coltrane)"]
    [:b {:class "btn btn-default"
         :on-click c-generate-pattern}
     "Generate"]
    [c-button]
    [:br]
    [:br]
-   [:p "(Use headphones and Chrome/Firefox for desktop or Safari/Chrome on mobile.)"]
    [:br]
    [:p "Made with ClojureScript, Tone.js, and Nightcoders.net"]
    [:a {:href "https://github.com/vossd/gs-pattern-gen"} "code"]])
 
 
 (r/render-component [content] (.querySelector js/document "#content"))
-
