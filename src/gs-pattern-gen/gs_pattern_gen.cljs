95ede82beceac8589686b1b7210850025d3b2f83
Dan <danvossmusic@gmail.com>
2018-01-21 12:21:42 -0500

    Refactor

--- src/gs-pattern-gen/gs_pattern_gen.cljs
+++ src/gs-pattern-gen/gs_pattern_gen.cljs
@@ -3,57 +3,53 @@
             [tonejs]))
 
 
-;;define chords
-;;using :C0 for rests
-(def b-maj [:B4 :C#5 :D#5 :F#5])
-(def g-maj [:G5 :A5 :B5 :D5])
-(def eb-maj [:Eb5 :F5 :G5 :Bb4])
-(def bb-maj [:Bb4 :C5 :D5 :F5])
-(def ab-maj [:Ab5 :Bb5 :C5 :Eb5])
-(def fis-maj [:F#5 :G#5 :A#5 :C#5])
-(def e-maj [:E5 :F#5 :G#5 :B4])
-(def d-maj [:D5 :E5 :F#5 :A5])
+;;define chords, using :C0 for rests
 (def c-maj [:C5 :D5 :E5 :G5])
+(def d-maj [:D5 :E5 :F#5 :A5])
+(def eb-maj [:Eb5 :F5 :G5 :Bb4])
+(def e-maj [:E5 :F#5 :G#5 :B4])
+(def fis-maj [:F#5 :G#5 :A#5 :C#5])
+(def g-maj [:G5 :A5 :B5 :D5])
+(def ab-maj [:Ab5 :Bb5 :C5 :Eb5])
+(def bb-maj [:Bb4 :C5 :D5 :F5])
+(def b-maj [:B4 :C#5 :D#5 :F#5])
 
-(def d-dom [:D5 :E5 :F#5 :A5 :C5 :C0])
-(def bb-dom [:Bb4 :C5 :D5 :F5 :Ab5 :C0])
-(def fis-dom [:F#5 :G#5 :A#4 :C#5 :E5 :C0])
-(def f-dom [:F5 :G5 :A5 :C5 :Eb5 :C0])
-(def eb-dom [:Eb5 :F5 :G5 :Bb5 :Db5 :C0])
 (def db-dom [:Db5 :Eb5 :F5 :Ab5 :B5 :C0])
-(def b-dom [:B4 :C#5 :D#5 :F#5 :A5 :C0])
-(def a-dom [:A5 :B4 :C#5 :E5 :G5 :C0])
+(def d-dom [:D5 :E5 :F#5 :A5 :C5 :C0])
+(def eb-dom [:Eb5 :F5 :G5 :Bb5 :Db5 :C0])
+(def f-dom [:F5 :G5 :A5 :C5 :Eb5 :C0])
+(def fis-dom [:F#5 :G#5 :A#4 :C#5 :E5 :C0])
 (def g-dom [:G5 :A5 :B4 :D5 :F5 :C0])
+(def a-dom [:A5 :B4 :C#5 :E5 :G5 :C0])
+(def bb-dom [:Bb4 :C5 :D5 :F5 :Ab5 :C0])
+(def b-dom [:B4 :C#5 :D#5 :F#5 :A5 :C0])
 
-(def a-min7 [:A4 :C5 :E5 :G5])
-(def f-min7 [:F5 :Ab5 :C5 :Eb5])
 (def cis-min7 [:C#5 :E5 :G#5 :B4])
+(def f-min7 [:F5 :Ab5 :C5 :Eb5])
+(def a-min7 [:A4 :C5 :E5 :G5])
 
-(def eb-maj7 [:Eb5 :F5 :G5 :Bb4 :C5 :D5 :C0])
-(def b-maj7 [:B4 :C#5 :D#5 :F#5 :G#5 :A#5 :C0])
-(def g-maj7 [:G5 :A5 :B5 :D5 :E5 :F#5 :C0])
-(def d-maj7 [:D5 :E5 :F#5 :A5 :B5 :C#5 :C0])
 (def c-maj7 [:C5 :D5 :E5 :G5 :A5 :B5 :C0])
+(def d-maj7 [:D5 :E5 :F#5 :A5 :B5 :C#5 :C0])
+(def eb-maj7 [:Eb5 :F5 :G5 :Bb4 :C5 :D5 :C0])
+(def g-maj7 [:G5 :A5 :B5 :D5 :E5 :F#5 :C0])
 (def bb-maj7 [:Bb4 :C5 :D5 :F5 :G5 :A5 :C0])
+(def b-maj7 [:B4 :C#5 :D#5 :F#5 :G#5 :A#5 :C0])
 
-(def e-min [:E5 :F#5 :G5 :A5])
-(def d-min [:D5 :E5 :F5 :G5])
 (def c-min [:C5 :D5 :Eb5 :F5])
+(def d-min [:D5 :E5 :F5 :G5])
+(def e-min [:E5 :F#5 :G5 :A5])
 
+(def eb-wt [:Eb5 :F5 :G5 :A5 :Db5 :C0])
 (def e-dyad [:A5 :C6 :C0])
 (def f-dom7 [:F5 :A5 :C5 :Eb5])
-(def eb-wt [:Eb5 :F5 :G5 :A5 :Db5 :C0])
 
 
 ;;create individual chord patterns
 (defn create-from-tetrad [tetrad]
   (shuffle tetrad))
 
-(defn create-from-pentad [pentad]
-  (vec (take 4 (shuffle pentad))))
-
-(defn create-from-hexad [hexad]
-  (vec (take 4 (shuffle hexad))))
+(defn create-from-chord [chord]
+  (vec (take 4 (shuffle chord))))
 
 (defn create-from-dyad [dyad]
   (flatten (concat (repeatedly 4 #(take 2 (shuffle dyad))))))
@@ -64,37 +60,37 @@
   (let [gs-group
         (clj->js (flatten (repeatedly 9 #(vec (concat
                                                 (create-from-tetrad b-maj)
-                                                (create-from-pentad d-dom)
+                                                (create-from-chord d-dom)
                                                 (create-from-tetrad g-maj)
-                                                (create-from-pentad bb-dom)
+                                                (create-from-chord bb-dom)
                                                 (create-from-tetrad eb-maj)
-                                                (create-from-hexad eb-maj7)
+                                                (create-from-chord eb-maj7)
                                                 (create-from-tetrad a-min7)
-                                                (create-from-pentad d-dom)
+                                                (create-from-chord d-dom)
                                                 (create-from-tetrad g-maj)
-                                                (create-from-pentad bb-dom)
+                                                (create-from-chord bb-dom)
                                                 (create-from-tetrad eb-maj)
-                                                (create-from-pentad fis-dom)
+                                                (create-from-chord fis-dom)
                                                 (create-from-tetrad b-maj)
-                                                (create-from-hexad b-maj7)
+                                                (create-from-chord b-maj7)
                                                 (create-from-tetrad f-min7)
-                                                (create-from-pentad bb-dom)
+                                                (create-from-chord bb-dom)
                                                 (create-from-tetrad eb-maj)
-                                                (create-from-pentad eb-maj7)
+                                                (create-from-chord eb-maj7)
                                                 (create-from-tetrad a-min7)
-                                                (create-from-pentad d-dom)
+                                                (create-from-chord d-dom)
                                                 (create-from-tetrad g-maj)
-                                                (create-from-pentad g-maj7)
+                                                (create-from-chord g-maj7)
                                                 (create-from-tetrad cis-min7)
-                                                (create-from-pentad fis-dom)
+                                                (create-from-chord fis-dom)
                                                 (create-from-tetrad b-maj)
-                                                (create-from-pentad b-maj7)
+                                                (create-from-chord b-maj7)
                                                 (create-from-tetrad f-min7)
-                                                (create-from-pentad bb-dom)
+                                                (create-from-chord bb-dom)
                                                 (create-from-tetrad eb-maj)
-                                                (create-from-pentad eb-maj7)
+                                                (create-from-chord eb-maj7)
                                                 (create-from-tetrad cis-min7)
-                                                (create-from-pentad fis-dom))))))]
+                                                (create-from-chord fis-dom))))))]
     gs-group))
 
 
@@ -103,36 +99,36 @@
   (let [c-group
         (clj->js (flatten (repeatedly 9 #(vec (concat
                                                 (create-from-tetrad e-min)
-                                                (create-from-pentad f-dom)
+                                                (create-from-chord f-dom)
                                                 (create-from-tetrad bb-maj)
-                                                (create-from-pentad db-dom)
+                                                (create-from-chord db-dom)
                                                 (create-from-tetrad fis-maj)
-                                                (create-from-pentad a-dom)
+                                                (create-from-chord a-dom)
                                                 (create-from-tetrad d-maj)
-                                                (create-from-hexad d-maj7)
+                                                (create-from-chord d-maj7)
                                                 (create-from-tetrad d-min)
-                                                (create-from-pentad eb-dom)
+                                                (create-from-chord eb-dom)
                                                 (create-from-tetrad ab-maj)
-                                                (create-from-pentad b-dom)
+                                                (create-from-chord b-dom)
                                                 (create-from-tetrad e-maj)
-                                                (create-from-pentad g-dom)
+                                                (create-from-chord g-dom)
                                                 (create-from-tetrad c-maj)
-                                                (create-from-hexad c-maj7)
+                                                (create-from-chord c-maj7)
                                                 (create-from-tetrad c-min)
-                                                (create-from-pentad db-dom)
+                                                (create-from-chord db-dom)
                                                 (create-from-tetrad fis-maj)
-                                                (create-from-pentad a-dom)
+                                                (create-from-chord a-dom)
                                                 (create-from-tetrad d-maj)
-                                                (create-from-pentad f-dom)
+                                                (create-from-chord f-dom)
                                                 (create-from-tetrad bb-maj)
-                                                (create-from-hexad bb-maj7)
+                                                (create-from-chord bb-maj7)
                                                 (create-from-dyad e-dyad)
                                                 (create-from-tetrad f-dom7)
-                                                (create-from-pentad f-dom)
+                                                (create-from-chord f-dom)
                                                 (create-from-tetrad bb-maj)
-                                                (create-from-hexad bb-maj7)
-                                                (create-from-pentad eb-wt)
-                                                (create-from-pentad eb-wt))))))]
+                                                (create-from-chord bb-maj7)
+                                                (create-from-chord eb-wt)
+                                                (create-from-chord eb-wt))))))]
     c-group))
 
 
@@ -161,59 +157,34 @@
                     (.toMaster)))
 
 
-;;create the lead sequences
-(defn gs-create-sequence [gs-group]
-  (let [gs-sequence (-> (new js/Tone.Sequence
-                          (fn [time note]
-                            (.triggerAttackRelease treble-synth note "4n" time))
-                          gs-group "8n")
-                        (.start 0))]
-    gs-sequence))
-
-(defn c-create-sequence [c-group]
-  (let [c-sequence (-> (new js/Tone.Sequence
-                         (fn [time note]
-                           (.triggerAttackRelease treble-synth note "4n" time))
-                         c-group "8n")
-                       (.start 0))]
-    c-sequence))
+;;create the lead sequence
+(defn create-sequence [group]
+  (let [sequence (-> (new js/Tone.Sequence
+                       (fn [time note]
+                         (.triggerAttackRelease treble-synth note "4n" time))
+                       group "8n")
+                     (.start 0))]
+    sequence))
 
 
-;;generate the complete synth patterns
-(defn gs-generate-pattern []
-  (let [gs-group (make-gs-group)
-        gs-sequence (gs-create-sequence gs-group)]
-    gs-sequence
-    (set! (.-loop gs-sequence) 1)))
-
-(defn c-generate-pattern []
-  (let [c-group (make-c-group)
-        c-sequence (c-create-sequence c-group)]
-    c-sequence
-    (set! (.-loop c-sequence) 1)))
+;;generate the complete synth pattern
+(defn generate-pattern [make-group]
+  (let [group (make-group)
+        sequence (create-sequence group)]
+    sequence
+    (set! (.-loop sequence) 1)))
 
 
 ;;start and stop the audio transport
-(defn gs-play []
-  (let [gs-bass-seq (-> (new js/Tone.Sequence
-                          (fn [time note]
-                            (.triggerAttackRelease bass-synth note "4n" time))
-                          gs-bass-group "2n")
-                        (.start 0))]
-    gs-bass-seq
-    (set! (.-loop gs-bass-seq) 9))
-  (set! (.-value js/Tone.Transport.bpm) 200)
-  (.start js/Tone.Transport "+0.2"))
-
-(defn c-play []
-  (let [c-bass-seq (-> (new js/Tone.Sequence
-                         (fn [time note]
-                           (.triggerAttackRelease bass-synth note "4n" time))
-                         c-bass-group "2n")
-                       (.start 0))]
-    c-bass-seq
-    (set! (.-loop c-bass-seq) 9))
-  (set! (.-value js/Tone.Transport.bpm) 240)
+(defn play [bass-group tempo]
+  (let [bass-seq (-> (new js/Tone.Sequence
+                       (fn [time note]
+                         (.triggerAttackRelease bass-synth note "4n" time))
+                       bass-group "2n")
+                     (.start 0))]
+    bass-seq
+    (set! (.-loop bass-seq) 9))
+  (set! (.-value js/Tone.Transport.bpm) tempo)
   (.start js/Tone.Transport "+0.2"))
 
 
@@ -221,41 +192,25 @@
   (.stop js/Tone.Transport))
 
 
-(defn gs-toggle [a t1 t2]
+;;make the play button toggle-able
+(defn toggle [a t1 t2 bass-group tempo]
   (if (= @a t1)
     (do
       (reset! a t2)
-      (gs-play))
-    (do
-      (reset! a t1)
-      (stop-func))))
-
-(defn c-toggle [a t1 t2]
-  (if (= @a t1)
-    (do
-      (reset! a t2)
-      (c-play))
+      (play bass-group tempo))
     (do
       (reset! a t1)
       (stop-func))))
 
 
-(defn gs-button []
+;;play button takes the bass group and corresponding tempo as arguments
+(defn button [bass-group tempo]
   (let [button-text (r/atom "Play")]
     (fn []
       [:div
        [:button
         {:class "btn btn-default"
-         :on-click #(gs-toggle button-text "Play" "Stop")}
-        (deref button-text)]])))
-
-(defn c-button []
-  (let [button-text (r/atom "Play")]
-    (fn []
-      [:div
-       [:button
-        {:class "btn btn-default"
-         :on-click #(c-toggle button-text "Play" "Stop")}
+         :on-click #(toggle button-text "Play" "Pause" bass-group tempo)}
         (deref button-text)]])))
 
 
@@ -265,16 +220,16 @@
    [:br]
    [:p "Giant Steps (John Coltrane)"]
    [:b {:class "btn btn-default"
-        :on-click gs-generate-pattern}
+        :on-click #(generate-pattern make-gs-group)}
     "Generate"]
-   [gs-button]
+   [button gs-bass-group 200]
    [:br]
    [:br]
    [:p "Countdown (John Coltrane)"]
    [:b {:class "btn btn-default"
-        :on-click c-generate-pattern}
+        :on-click #(generate-pattern make-c-group)}
     "Generate"]
-   [c-button]
+   [button c-bass-group 240]
    [:br]
    [:br]
    [:br]
